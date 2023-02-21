4cee1a91538f4e810569fc5eb6319ab99ac76a57
BrendanVotano <78179739+JMC-Academy@users.noreply.github.com>
2023-02-21 12:02:30 +1100

    Win Condition & Timer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,20 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody rb;
     public float speed = 1;
+    int pickupCount;
+    Timer timer;
 
     void Start()
     {
         //Get the rigidbody component of the gameObject
         rb = GetComponent<Rigidbody>();
+        //Get the number of pickups in our scene
+        pickupCount = GameObject.FindGameObjectsWithTag("Pickup").Length;
+        //Display the pickup count
+        CheckPickups();
+        //Get the Timer object and start the timer
+        timer = FindObjectOfType<Timer>();
+        timer.StartTimer();
     }
 
     void Update()
@@ -26,4 +35,35 @@ public class PlayerController : MonoBehaviour
         //Add force to the rigidbody based on our movement vector
         rb.AddForce(movement * speed);
     }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        //If the other object contains the Pickup tag, destroy it
+        if(other.CompareTag("Pickup"))
+        {
+            Destroy(other.gameObject);
+            //Decrement the pickup count
+            pickupCount -= 1;
+            CheckPickups();
+        }
+    }
+
+    void CheckPickups()
+    {
+        Debug.Log("Pickups left: " + pickupCount);
+
+        if (pickupCount == 0)
+        {
+            WinGame();
+        }
+    }
+
+    void WinGame()
+    {
+        timer.StopTimer();
+        Debug.Log("You Win!!! Your time was: " + timer.GetTime().ToString("F3"));
+    }
+
+
 }
