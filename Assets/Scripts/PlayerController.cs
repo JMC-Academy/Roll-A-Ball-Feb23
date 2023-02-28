62aa8d3153c4f7c7d6573a2aea25a2a8db8390f7
BrendanVotano <78179739+JMC-Academy@users.noreply.github.com>
2023-02-28 11:59:57 +1100

    Week 3 - UI

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -7,23 +8,41 @@ public class PlayerController : MonoBehaviour
     Rigidbody rb;
     public float speed = 1;
     int pickupCount;
+    int totalPickups;
     Timer timer;
+    bool wonGame = false;
+
+    [Header("UI")]
+    public GameObject winPanel;
+    public TMP_Text winTime;
+    public GameObject inGamePanel;
+    public TMP_Text timerText;
+    public TMP_Text pickupText;
 
     void Start()
     {
         //Get the rigidbody component of the gameObject
         rb = GetComponent<Rigidbody>();
         //Get the number of pickups in our scene
-        pickupCount = GameObject.FindGameObjectsWithTag("Pickup").Length;
+        totalPickups = GameObject.FindGameObjectsWithTag("Pickup").Length;
+        //Set the pickup count to the total
+        pickupCount = totalPickups;
         //Display the pickup count
         CheckPickups();
         //Get the Timer object and start the timer
         timer = FindObjectOfType<Timer>();
         timer.StartTimer();
+        //Turn off our win panel
+        winPanel.SetActive(false);
+        //Turn on our in game panel
+        inGamePanel.SetActive(true);
     }
 
     void Update()
     {
+        if (wonGame == true)
+            return;
+
         //Get the input value from our horizontal axis
         float moveHorizontal = Input.GetAxis("Horizontal");
         //Get the input value from our vertical axis
@@ -34,6 +53,9 @@ public class PlayerController : MonoBehaviour
 
         //Add force to the rigidbody based on our movement vector
         rb.AddForce(movement * speed);
+
+        //Update the timer text to that of the timer
+        timerText.text = "Time: " + timer.GetTime().ToString("F3");
     }
 
 
@@ -51,8 +73,8 @@ public class PlayerController : MonoBehaviour
 
     void CheckPickups()
     {
-        Debug.Log("Pickups left: " + pickupCount);
-
+        //Debug.Log("Pickups left: " + pickupCount);
+        pickupText.text = "Pickups left: " + pickupCount + "/" + totalPickups;
         if (pickupCount == 0)
         {
             WinGame();
@@ -61,9 +83,25 @@ public class PlayerController : MonoBehaviour
 
     void WinGame()
     {
+        wonGame = true;
         timer.StopTimer();
-        Debug.Log("You Win!!! Your time was: " + timer.GetTime().ToString("F3"));
+        //Debug.Log("You Win!!! Your time was: " + timer.GetTime().ToString("F3"));
+        //Turn off our in game panel
+        inGamePanel.SetActive(false);
+        //Set the timer on the text
+        winTime.text = "Your time was: " + timer.GetTime().ToString("F3");
+        //Turn on our win panel
+        winPanel.SetActive(true);
+
+        //Set the velocity of the rigidbody to zero
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
     }
 
+    //Temporary - Remove when doing modules in A2
+    public void RestartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
 
 }
